a6d238a1a19bfa4c3e0052d02a63fd6e2e734521
Krishna Kaviarasu S <krishnakaviarusu03@gmail.com>
2024-08-28 11:48:19 +0530

    git init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import'./App.css'
+import { faChessPawn,faChessRook,faChessBishop,faChessKing,faChessKnight,faChessQueen } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='board'>
+      <table>
+        <tr>
+          <th className='white key' ><FontAwesomeIcon className='bk' icon={faChessRook} /></th>
+          <th className='key'><FontAwesomeIcon className='bk' icon={faChessKnight} /></th>
+          <th className='white key'><FontAwesomeIcon className='bk' icon={faChessBishop} /></th>
+          <th className='key'><FontAwesomeIcon className='bk' icon={faChessQueen} /></th>
+          <th className='white key'><FontAwesomeIcon className='bk' icon={faChessKing} /></th>
+          <th className='key'><FontAwesomeIcon className='bk' icon={faChessBishop} /></th>
+          <th className='white key'><FontAwesomeIcon className='bk' icon={faChessKnight} /></th>
+          <th className='key'><FontAwesomeIcon className='bk' icon={faChessRook} /></th>
+        </tr>
+        <tr>
+          <th className='key'><FontAwesomeIcon className='bk' icon={faChessPawn} /></th>
+          <th className='white key'><FontAwesomeIcon className='bk' icon={faChessPawn} /></th>
+          <th className='key'><FontAwesomeIcon className='bk' icon={faChessPawn} /></th>
+          <th className='white key'><FontAwesomeIcon className='bk' icon={faChessPawn} /></th>
+          <th className='key'><FontAwesomeIcon className='bk' icon={faChessPawn} /></th>
+          <th className='white key'><FontAwesomeIcon className='bk' icon={faChessPawn} /></th>
+          <th className='key'><FontAwesomeIcon className='bk' icon={faChessPawn} /></th>
+          <th className='white key'><FontAwesomeIcon className='bk' icon={faChessPawn} /></th>
+        </tr>
+        <tr>
+          <th className='white'></th>
+          <th></th>
+          <th className='white'></th>
+          <th></th>
+          <th className='white'></th>
+          <th></th>
+          <th className='white'></th>
+          <th></th>
+        </tr>
+        <tr>
+          <th></th>
+          <th className='white'></th>
+          <th></th>
+          <th className='white'></th>
+          <th></th>
+          <th className='white'></th>
+          <th></th>
+          <th className='white'></th>
+        </tr><tr>
+          <th className='white'></th>
+          <th></th>
+          <th className='white'></th>
+          <th></th>
+          <th className='white'></th>
+          <th></th>
+          <th className='white'></th>
+          <th></th>
+        </tr>
+        <tr>
+          <th></th>
+          <th className='white'></th>
+          <th></th>
+          <th className='white'></th>
+          <th></th>
+          <th className='white'></th>
+          <th></th>
+          <th className='white'></th>
+        </tr><tr>
+          <th className='white key'><FontAwesomeIcon className='wk' icon={faChessPawn} style={{color: "#ffffff",}} /></th>
+          <th className='key'><FontAwesomeIcon className='wk'icon={faChessPawn} style={{color: "#ffffff",}} /></th>
+          <th className='white key'><FontAwesomeIcon className='wk' icon={faChessPawn} style={{color: "#ffffff",}} /></th>
+          <th className='key'><FontAwesomeIcon className='wk' icon={faChessPawn} style={{color: "#ffffff",}} /></th>
+          <th className='white key'><FontAwesomeIcon className='wk' icon={faChessPawn} style={{color: "#ffffff",}} /></th>
+          <th className='key'><FontAwesomeIcon className='wk' icon={faChessPawn} style={{color: "#ffffff",}} /></th>
+          <th className='white key'><FontAwesomeIcon className='wk' icon={faChessPawn} style={{color: "#ffffff",}} /></th>
+          <th className='key'><FontAwesomeIcon className='wk' icon={faChessPawn} style={{color: "#ffffff",}} /></th>
+        </tr>
+        <tr>
+          <th className='key'><FontAwesomeIcon className='wk' icon={faChessRook} style={{color: "#ffffff",}} /></th>
+          <th className='white key'><FontAwesomeIcon className='wk' icon={faChessKnight} style={{color: "#ffffff",}} /></th>
+          <th className='key'><FontAwesomeIcon className='wk' icon={faChessBishop} style={{color: "#ffffff",}} /></th>
+          <th className='white key'><FontAwesomeIcon className='wk' icon={faChessQueen} style={{color: "#ffffff",}} /></th>
+          <th className='key'><FontAwesomeIcon className='wk' icon={faChessKing} style={{color: "#ffffff",}} /></th>
+          <th className='white key'><FontAwesomeIcon className='wk' icon={faChessBishop} style={{color: "#ffffff",}} /></th>
+          <th className='key'><FontAwesomeIcon className='wk' icon={faChessKnight} style={{color: "#ffffff",}} /></th>
+          <th className='white key'><FontAwesomeIcon className='wk' icon={faChessRook} style={{color: "#ffffff",}} /></th>
+        </tr>
+
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
